cdc8135ad67fdeacfe5eb1727f551082977d8d13
zcy <1957905400@qq.com>
2020-04-13 23:53:23 +0800

    第一更新

---
+++ src/main/java/com/example/vvv/VvvApplication.java
@@ -0,0 +1,13 @@
+package com.example.vvv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VvvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VvvApplication.class, args);
+    }
+
+}
